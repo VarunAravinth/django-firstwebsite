9561307500fc4012ecf3d421cd2af0f61211110e
VarunAravinth <varunaravinth28@gmail.com>
2018-08-26 01:23:23 +0530

    new html templates created and hooked up with front end

--- music/views.py
+++ music/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Album
 
 
 def index(request):
-    return HttpResponse('<h1>THIS IS A STATIC11 PAGE</h1>')
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums, }
+    return render(request, 'music/index.html', context)
+
+
+def detail(request, album_id):
+    return HttpResponse('<h2> Album returned with id : '+album_id+' </h2>')
 
