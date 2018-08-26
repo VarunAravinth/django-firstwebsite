4f4145ca528fb49931b60c173167d384f0bc6c61
VarunAravinth <varunaravinth28@gmail.com>
2018-08-26 20:56:58 +0530

    details page and index page progressed, added songs under albums and handled non existing id with 404 exception

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Album
@@ -7,10 +8,13 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {'all_albums': all_albums, }
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse('<h2> Album returned with id : '+album_id+' </h2>')
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
 
