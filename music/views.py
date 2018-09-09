cf456b5e9a8bde32377865921ee46bdd6f0102e4
VarunAravinth <varunaravinth28@gmail.com>
2018-09-10 01:18:19 +0530

    added favorite function

--- music/views.py
+++ music/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import Http404,get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -14,4 +13,22 @@ def index(request):
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': 'Select a valid song',
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+
+
 
